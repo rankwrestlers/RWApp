e58f6370d9da250fc7651d343843d41fcd152e11
Kirk Briden <admin@rankwrestlers.com>
2019-10-05 16:14:25 -0500

    Initial Commit

---
+++ RWApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RWApp
+//
+//  Created by RankWresters on 10/5/19.
+//  Copyright © 2019 RankWresters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
